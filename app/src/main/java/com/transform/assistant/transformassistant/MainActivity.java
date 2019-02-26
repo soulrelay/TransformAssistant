199691c2c2dbf975e066dfea4932915bfa3673e1
sushuai <sushuai@didiglobal.com>
2019-02-26 11:47:13 +0800

    Initial commit

---
+++ app/src/main/java/com/transform/assistant/transformassistant/MainActivity.java
@@ -0,0 +1,13 @@
+package com.transform.assistant.transformassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
